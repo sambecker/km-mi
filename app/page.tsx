f3e0e76a6746b1c516fb284ed854de6de04ed4f6
Sam Becker <sam@sambecker.com>
2024-04-01 18:16:46 -0500

    Wrap useSearchParams in Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import ClientInputs from "@/components/ClientInputs";
 import ClientHeader from "@/components/ClientHeader";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main>
       <div className="text-8xl font-bold flex w-full items-center justify-center h-[100vh]">
         <div className="translate-y-[-1rem] space-y-4">
-          <ClientHeader />
-          <ClientInputs />
+          <Suspense>
+            <ClientHeader />
+            <ClientInputs />
+          </Suspense>
         </div>
       </div>
     </main>
