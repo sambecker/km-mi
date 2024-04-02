c043f8d9c48b28c07bb5f167e7a31a637b738fff
Sam Becker <sam@sambecker.com>
2024-04-02 12:26:17 -0500

    Introduce arrows

--- components/ClientHeader.tsx
+++ components/ClientHeader.tsx
@@ -4,9 +4,9 @@ import { useAppState } from '@/state';
 import { twMerge } from 'tailwind-merge';
 
 export default function ClientHeader() {
-  const { unit, values } = useAppState();
+  const { unit } = useAppState();
 
-  const renderFadingSegment = (text: string, shouldFade: boolean) =>
+  const renderFadingSegment = (text: string, shouldFade?: boolean) =>
     <span className={twMerge(
       'transition-opacity duration-300',
       shouldFade && 'opacity-10',
@@ -15,9 +15,9 @@ export default function ClientHeader() {
     </span>;
 
   return (
-    <div>
+    <div className="flex gap-2">
       {renderFadingSegment('KM', unit !== 'km')}
-      {renderFadingSegment('/', values == undefined)}
+      {renderFadingSegment(unit === 'km' ? '→' : unit === 'mi' ? '←' : '/')}
       {renderFadingSegment('MI', unit !== 'mi')}
     </div>
   );
